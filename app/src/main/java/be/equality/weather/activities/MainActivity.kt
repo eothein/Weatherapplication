3cb569e0717d1e2cff4bae5edc50ce287a2733e5
eothein <jens.buysse@gmail.com>
2018-09-12 19:13:22 +0200

    Added a network call in a synchronous fashion using the Anko Library.
    Added a network configuration file to overcome the "ClearText" network error.

--- app/src/main/java/be/equality/weather/activities/MainActivity.kt
+++ app/src/main/java/be/equality/weather/activities/MainActivity.kt
@@ -6,9 +6,9 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import be.equality.weather.R
 import be.equality.weather.adapters.ForecastListadapter
+import be.equality.weather.network.Request
 import com.mooveit.library.Fakeit
-import org.jetbrains.anko.find
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,11 +29,24 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+                "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
+
+
+        //IWe use the doAsync function that will execute its code
+        //in another thread, with the option to return to the main thread by calling uiThread.
+        doAsync {
+            Request(url).run()
+            uiThread { longToast("Request performed") }
+        }
+
+
         //as is the casting operator
         val forecastList : RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListadapter(list)
-        toast("Welcome to the application")
+
     }
 
 }
